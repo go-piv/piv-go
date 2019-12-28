f89d9d8f0a54e60964d98bed169c3b0af7bd7a3a
Eric Chiang <ericchiang@google.com>
2019-12-28 15:51:04 -0800

    piv: add code for getting a yubikey's serial number

--- ykpiv/ykpiv.go
+++ ykpiv/ykpiv.go
@@ -1,6 +1,9 @@
 package ykpiv
 
-import "fmt"
+import (
+	"encoding/binary"
+	"fmt"
+)
 
 /*
 union u_APDU {
@@ -15,6 +18,15 @@ union u_APDU {
   unsigned char raw[0xff + 5];
 };
 */
+
+func Smartcards() ([]string, error) {
+	ctx, err := newSCContext()
+	if err != nil {
+		return nil, fmt.Errorf("connecting to pscs: %v", err)
+	}
+	defer ctx.Close()
+	return ctx.ListReaders()
+}
 
 const (
 	// Blindly copied from ykpiv.h. Is this from the PIV spec?
@@ -38,8 +50,69 @@ const (
 	insGetSerial     = 0xf8
 )
 
-var aid = [...]byte{
-	0xa0, 0x00, 0x00, 0x03, 0x08,
+type Yubikey struct {
+	ctx *scContext
+	h   *scHandle
+
+	// Used to determine how to access certain functionality.
+	version *version
+}
+
+func (y *Yubikey) Close() error {
+	err1 := y.h.Close()
+	err2 := y.ctx.Close()
+	if err1 == nil {
+		return err2
+	}
+	return err1
+}
+
+func newYubikey(card string) (*Yubikey, error) {
+	ctx, err := newSCContext()
+	if err != nil {
+		return nil, fmt.Errorf("connecting to smartcard daemon: %v", err)
+	}
+
+	h, err := ctx.Connect(card)
+	if err != nil {
+		ctx.Close()
+		return nil, fmt.Errorf("connecting to smartcard: %v", err)
+	}
+
+	yk := &Yubikey{ctx: ctx, h: h}
+	tx, err := yk.begin()
+	if err != nil {
+		yk.Close()
+		return nil, fmt.Errorf("initializing yubikey: %v", err)
+	}
+	v, err := ykVersion(tx)
+	if err != nil {
+		yk.Close()
+		return nil, fmt.Errorf("getting yubikey version: %v", err)
+	}
+	yk.version = v
+	return yk, nil
+}
+
+func (yk *Yubikey) begin() (*scTx, error) {
+	tx, err := yk.h.Begin()
+	if err != nil {
+		return nil, fmt.Errorf("beginning smartcard transaction: %v", err)
+	}
+	if err := ykSelectApplication(tx, aidPIV[:]); err != nil {
+		tx.Close()
+		return nil, fmt.Errorf("selecting piv applet: %v", err)
+	}
+	return tx, nil
+}
+
+func (yk *Yubikey) Serial() (uint32, error) {
+	tx, err := yk.begin()
+	if err != nil {
+		return 0, err
+	}
+	defer tx.Close()
+	return ykSerial(tx, yk.version)
 }
 
 type version struct {
@@ -48,12 +121,23 @@ type version struct {
 	patch byte
 }
 
-func ykSelectApplication(tx *scTx) error {
-	// copied from https://github.com/Yubico/yubico-piv-tool/blob/yubico-piv-tool-1.7.0//lib/ykpiv.c#L273
+var (
+	// Smartcard Application IDs for Yubikeys.
+	//
+	// https://github.com/Yubico/yubico-piv-tool/blob/yubico-piv-tool-1.7.0/lib/ykpiv.c#L1877
+	// https://github.com/Yubico/yubico-piv-tool/blob/yubico-piv-tool-1.7.0/lib/ykpiv.c#L108-L110
+	// https://github.com/Yubico/yubico-piv-tool/blob/yubico-piv-tool-1.7.0/lib/ykpiv.c#L1117
+
+	aidManagement = [...]byte{0xa0, 0x00, 0x00, 0x05, 0x27, 0x47, 0x11, 0x17}
+	aidPIV        = [...]byte{0xa0, 0x00, 0x00, 0x03, 0x08}
+	aidYubikey    = [...]byte{0xa0, 0x00, 0x00, 0x05, 0x27, 0x20, 0x01, 0x01}
+)
+
+func ykSelectApplication(tx *scTx, id []byte) error {
 	cmd := adpu{
 		instruction: insSelectApplication,
 		param1:      0x04,
-		data:        aid[:],
+		data:        id[:],
 	}
 	if _, err := tx.Transmit(cmd); err != nil {
 		return fmt.Errorf("command failed: %v", err)
@@ -73,4 +157,25 @@ func ykVersion(tx *scTx) (*version, error) {
 		return nil, fmt.Errorf("response was too short: %d", n)
 	}
 	return &version{resp[0], resp[1], resp[2]}, nil
+}
+
+func ykSerial(tx *scTx, v *version) (uint32, error) {
+	cmd := adpu{instruction: insGetSerial}
+	if v.major < 5 {
+		// Earlier versions of Yubikeys required using the yubikey applet to get
+		// the serial number. Newer ones have this built into the PIV applet.
+		if err := ykSelectApplication(tx, aidYubikey[:]); err != nil {
+			return 0, fmt.Errorf("selecting yubikey applet: %v", err)
+		}
+		defer ykSelectApplication(tx, aidPIV[:])
+		cmd = adpu{instruction: 0x01, param1: 0x10}
+	}
+	resp, err := tx.Transmit(cmd)
+	if err != nil {
+		return 0, fmt.Errorf("smartcard command: %v", err)
+	}
+	if n := len(resp); n != 4 {
+		return 0, fmt.Errorf("expected 4 byte serial number, got %d", n)
+	}
+	return binary.BigEndian.Uint32(resp), nil
 }
--- ykpiv/ykpiv_test.go
+++ ykpiv/ykpiv_test.go
@@ -8,14 +8,52 @@ func testGetVersion(t *testing.T, h *scHandle) {
 		t.Fatalf("new transaction: %v", err)
 	}
 	defer tx.Close()
-	if err := ykSelectApplication(tx); err != nil {
+	if err := ykSelectApplication(tx, aidPIV[:]); err != nil {
 		t.Fatalf("selecting application: %v", err)
 	}
-	v, err := ykVersion(tx)
-	if err != nil {
+	if _, err := ykVersion(tx); err != nil {
 		t.Fatalf("listing version: %v", err)
 	}
-	t.Logf("%#v", v)
 }
 
 func TestGetVersion(t *testing.T) { runHandleTest(t, testGetVersion) }
+
+func TestSmartcards(t *testing.T) {
+	if _, err := Smartcards(); err != nil {
+		t.Fatalf("listing cards: %v", err)
+	}
+}
+
+func newTestYubikey(t *testing.T) (*Yubikey, func()) {
+	cards, err := Smartcards()
+	if err != nil {
+		t.Fatalf("listing cards: %v", err)
+	}
+	for _, card := range cards {
+		yk, err := newYubikey(card)
+		if err != nil {
+			t.Fatalf("getting new yubikey: %v", err)
+		}
+		return yk, func() {
+			if err := yk.Close(); err != nil {
+				t.Errorf("closing yubikey: %v", err)
+			}
+		}
+	}
+	t.Skip("no yubikeys detected, skipping")
+	return nil, nil
+}
+
+func TestNewYubikey(t *testing.T) {
+	_, close := newTestYubikey(t)
+	defer close()
+}
+
+func TestYubikeySerial(t *testing.T) {
+	yk, close := newTestYubikey(t)
+	defer close()
+
+	if _, err := yk.Serial(); err != nil {
+		t.Fatalf("getting serial number: %v", err)
+	}
+}
