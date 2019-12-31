5aa7d740ee8cc360a228646d4ee1019f4a135094
Eric Chiang <ericchiang@google.com>
2019-12-30 17:50:04 -0800

    ykpiv: add code for authenticating and changing management key

--- ykpiv/ykpiv.go
+++ ykpiv/ykpiv.go
@@ -1,29 +1,26 @@
 package ykpiv
 
 import (
+	"bytes"
+	"crypto/des"
+	"crypto/rand"
 	"encoding/binary"
 	"errors"
 	"fmt"
+	"io"
 )
 
-/*
-union u_APDU {
-  struct {
-    unsigned char cla;
-    unsigned char ins;
-    unsigned char p1;
-    unsigned char p2;
-    unsigned char lc;
-    unsigned char data[0xff];
-  } st;
-  unsigned char raw[0xff + 5];
-};
-*/
-
 const (
-	DefaultPIN           = "123456"
-	DefaultPUK           = "12345678"
-	DefaultManagementKey = "010203040506070801020304050607080102030405060708"
+	DefaultPIN = "123456"
+	DefaultPUK = "12345678"
+)
+
+var (
+	DefaultManagementKey = [24]byte{
+		0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+		0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+		0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+	}
 )
 
 // ErrWrongPIN is the error returned when a login attempt fails because of an
@@ -50,12 +47,27 @@ func Smartcards() ([]string, error) {
 }
 
 const (
-	// Blindly copied from ykpiv.h. Is this from the PIV spec?
+	// https://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-78-4.pdf#page=17
+	algTag     = 0x80
+	alg3DES    = 0x03
+	algRSA1024 = 0x06
+	algRSA2048 = 0x07
+	algECCP256 = 0x11
+	algECCP384 = 0x14
+
+	// https://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-78-4.pdf#page=16
+	keyAuthentication     = 0x9a
+	keyCardManagement     = 0x9b
+	keySignature          = 0x9c
+	keyKeyManagement      = 0x9d
+	keyCardAuthentication = 0x9e
+	keyAttestation        = 0xf9
+
 	insVerify             = 0x20
 	insChangeReference    = 0x24
 	insResetRetry         = 0x2c
 	insGenerateAsymmetric = 0x47
-	insAuthenticated      = 0x87
+	insAuthenticate       = 0x87
 	insGetData            = 0xcb
 	insPutData            = 0xdb
 	insSelectApplication  = 0xa4
@@ -227,6 +239,115 @@ var (
 	aidPIV        = [...]byte{0xa0, 0x00, 0x00, 0x03, 0x08}
 	aidYubikey    = [...]byte{0xa0, 0x00, 0x00, 0x05, 0x27, 0x20, 0x01, 0x01}
 )
+
+func ykAuthenticate(tx *scTx, key [24]byte) error {
+	// https://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-73-4.pdf#page=92
+	// https://tsapps.nist.gov/publication/get_pdf.cfm?pub_id=918402#page=114
+
+	// request a witness
+	cmd := adpu{
+		instruction: insAuthenticate,
+		param1:      alg3DES,
+		param2:      keyCardManagement,
+		data: []byte{
+			0x7c, // Dynamic Authentication Template tag
+			0x02, // Length of object
+			0x80, // 'Witness'
+			0x00, // Return encrypted random
+		},
+	}
+	resp, err := tx.Transmit(cmd)
+	if err != nil {
+		return fmt.Errorf("get auth challenge: %v", err)
+	}
+	if n := len(resp); n < 12 {
+		return fmt.Errorf("challenge didn't return enough bytes: %d", n)
+	}
+	if !bytes.Equal(resp[:4], []byte{
+		0x7c,
+		0x0a,
+		0x80, // 'Witness'
+		0x08, // Tag length
+	}) {
+		return fmt.Errorf("invalid authentication object header: %x", resp[:4])
+	}
+
+	cardChallenge := resp[4 : 4+8]
+	cardResponse := make([]byte, 8)
+
+	block, err := des.NewTripleDESCipher(key[:])
+	if err != nil {
+		return fmt.Errorf("creating triple des block cipher: %v", err)
+	}
+	block.Decrypt(cardResponse, cardChallenge)
+
+	challenge := make([]byte, 8)
+	if _, err := io.ReadFull(rand.Reader, challenge); err != nil {
+		return fmt.Errorf("reading rand data: %v", err)
+	}
+	response := make([]byte, 8)
+	block.Encrypt(response, challenge)
+
+	data := append([]byte{
+		0x7c, // Dynamic Authentication Template tag
+		20,   // 2+8+2+8
+		0x80, // 'Witness'
+		0x08, // Tag length
+	})
+	data = append(data, cardResponse...)
+	data = append(data,
+		0x81, // 'Challenge'
+		0x08, // Tag length
+	)
+	data = append(data, challenge...)
+
+	cmd = adpu{
+		instruction: insAuthenticate,
+		param1:      alg3DES,
+		param2:      keyCardManagement,
+		data:        data,
+	}
+	resp, err = tx.Transmit(cmd)
+	if err != nil {
+		return fmt.Errorf("auth challenge: %v", err)
+	}
+	if n := len(resp); n < 12 {
+		return fmt.Errorf("challenge response didn't return enough bytes: %d", n)
+	}
+	if !bytes.Equal(resp[:4], []byte{
+		0x7c,
+		0x0a,
+		0x82, // 'Response'
+		0x08,
+	}) {
+		return fmt.Errorf("response invalid authentication object header: %x", resp[:4])
+	}
+	if !bytes.Equal(resp[4:4+8], response) {
+		return fmt.Errorf("challenge failed")
+	}
+
+	return nil
+}
+
+// ykSetManagementKey updates the management key to a new key. This requires
+// authenticating with the existing management key.
+func ykSetManagementKey(tx *scTx, key [24]byte, touch bool) error {
+	cmd := adpu{
+		instruction: insSetMGMKey,
+		param1:      0xff,
+		param2:      0xff,
+		data: append([]byte{
+			alg3DES, keyCardManagement, 24,
+		}, key[:]...),
+	}
+	if touch {
+		cmd.param2 = 0xfe
+	}
+	if _, err := tx.Transmit(cmd); err != nil {
+		return fmt.Errorf("command failed: %v", err)
+	}
+	return nil
+}
 
 func ykSelectApplication(tx *scTx, id []byte) error {
 	cmd := adpu{
--- ykpiv/ykpiv_test.go
+++ ykpiv/ykpiv_test.go
@@ -1,6 +1,10 @@
 package ykpiv
 
-import "testing"
+import (
+	"crypto/rand"
+	"io"
+	"testing"
+)
 
 func testGetVersion(t *testing.T, h *scHandle) {
 	tx, err := h.Begin()
@@ -76,5 +80,54 @@ func TestYubikeyLogin(t *testing.T) {
 
 	if err := yk.Login(DefaultPIN); err != nil {
 		t.Fatalf("login: %v", err)
+	}
+}
+
+func TestYubikeyAuthenticate(t *testing.T) {
+	yk, close := newTestYubikey(t)
+	defer close()
+
+	tx, err := yk.begin()
+	if err != nil {
+		t.Fatalf("begin transaction: %v", err)
+	}
+	defer tx.Close()
+
+	if err := ykAuthenticate(tx, DefaultManagementKey); err != nil {
+		t.Errorf("authenticating: %v", err)
+	}
+}
+
+func TestYubikeySetManagementKey(t *testing.T) {
+	yk, close := newTestYubikey(t)
+	defer close()
+
+	var mgmtKey [24]byte
+	if _, err := io.ReadFull(rand.Reader, mgmtKey[:]); err != nil {
+		t.Fatalf("generating management key: %v", err)
+	}
+
+	tx, err := yk.begin()
+	if err != nil {
+		t.Fatalf("begin transaction: %v", err)
+	}
+	defer tx.Close()
+
+	if err := ykSetManagementKey(tx, mgmtKey, false); err == nil {
+		t.Errorf("set management key without authenticating, expected error")
+	}
+
+	if err := ykAuthenticate(tx, DefaultManagementKey); err != nil {
+		t.Errorf("authenticating: %v", err)
+	}
+
+	if err := ykSetManagementKey(tx, mgmtKey, false); err != nil {
+		t.Fatalf("setting management key: %v", err)
+	}
+	if err := ykAuthenticate(tx, mgmtKey); err != nil {
+		t.Errorf("authenticating with new management key: %v", err)
+	}
+	if err := ykSetManagementKey(tx, DefaultManagementKey, false); err != nil {
+		t.Fatalf("setting management key: %v", err)
 	}
 }
