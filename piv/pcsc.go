e7b0a8ba700aba09ab05d45d965e6addc43cc672
Tobias Kohlbau <tobias@kohlbau.de>
2020-05-01 22:26:12 +0200

    add initial windows support
    Adds support for windows via winscard. This CL leverages the
    winscard api via calling into the dll. For more information
    see https://docs.microsoft.com/en-us/windows/win32/api/winscard/.
    
    Fixes #56
    
    Signed-off-by: Tobias Kohlbau <tobias@kohlbau.de>

--- piv/pcsc.go
+++ piv/pcsc.go
@@ -14,25 +14,10 @@
 
 package piv
 
-// https://ludovicrousseau.blogspot.com/2010/04/pcsc-sample-in-c.html
-
-// TODO: Figure out if linux flags should use pkg-config instead
-
-// #cgo darwin LDFLAGS: -framework PCSC
-// #cgo linux CFLAGS: -I/usr/include/PCSC
-// #cgo linux LDFLAGS: -lpcsclite
-// #include <PCSC/winscard.h>
-// #include <PCSC/wintypes.h>
-import "C"
-
 import (
-	"bytes"
 	"errors"
 	"fmt"
-	"unsafe"
 )
-
-const rcSuccess = C.SCARD_S_SUCCESS
 
 type scErr struct {
 	// rc holds the return code for a given call.
@@ -136,121 +121,11 @@ func (a *apduErr) Unwrap() error {
 	return nil
 }
 
-type scContext struct {
-	ctx C.SCARDCONTEXT
-}
-
-func newSCContext() (*scContext, error) {
-	var ctx C.SCARDCONTEXT
-	rc := C.SCardEstablishContext(C.SCARD_SCOPE_SYSTEM, nil, nil, &ctx)
-	if err := scCheck(rc); err != nil {
-		return nil, err
-	}
-	return &scContext{ctx: ctx}, nil
-}
-
-func (c *scContext) Close() error {
-	return scCheck(C.SCardReleaseContext(c.ctx))
-}
-
-func (c *scContext) ListReaders() ([]string, error) {
-	var n C.DWORD
-	rc := C.SCardListReaders(c.ctx, nil, nil, &n)
-	// On Linux, the PC/SC daemon will return an error when no smart cards are
-	// available. Detect this and return nil with no smart cards instead.
-	//
-	// isRCNoReaders is defined in OS specific packages.
-	if isRCNoReaders(rc) {
-		return nil, nil
-	}
-
-	if err := scCheck(rc); err != nil {
-		return nil, err
-	}
-
-	d := make([]byte, n)
-	rc = C.SCardListReaders(c.ctx, nil, (*C.char)(unsafe.Pointer(&d[0])), &n)
-	if err := scCheck(rc); err != nil {
-		return nil, err
-	}
-
-	var readers []string
-	for _, d := range bytes.Split(d, []byte{0}) {
-		if len(d) > 0 {
-			readers = append(readers, string(d))
-		}
-	}
-	return readers, nil
-}
-
-type scHandle struct {
-	h C.SCARDHANDLE
-}
-
-func (c *scContext) Connect(reader string) (*scHandle, error) {
-	var (
-		handle         C.SCARDHANDLE
-		activeProtocol C.DWORD
-	)
-	rc := C.SCardConnect(c.ctx, C.CString(reader),
-		C.SCARD_SHARE_EXCLUSIVE, C.SCARD_PROTOCOL_T1,
-		&handle, &activeProtocol)
-	if err := scCheck(rc); err != nil {
-		return nil, err
-	}
-	return &scHandle{handle}, nil
-}
-
-func (h *scHandle) Close() error {
-	return scCheck(C.SCardDisconnect(h.h, C.SCARD_LEAVE_CARD))
-}
-
-type scTx struct {
-	h C.SCARDHANDLE
-}
-
-func (h *scHandle) Begin() (*scTx, error) {
-	if err := scCheck(C.SCardBeginTransaction(h.h)); err != nil {
-		return nil, err
-	}
-	return &scTx{h.h}, nil
-}
-
-func (t *scTx) Close() error {
-	return scCheck(C.SCardEndTransaction(t.h, C.SCARD_LEAVE_CARD))
-}
-
 type apdu struct {
 	instruction byte
 	param1      byte
 	param2      byte
 	data        []byte
-}
-
-func (t *scTx) transmit(req []byte) (more bool, b []byte, err error) {
-	var resp [C.MAX_BUFFER_SIZE_EXTENDED]byte
-	reqN := C.DWORD(len(req))
-	respN := C.DWORD(len(resp))
-	rc := C.SCardTransmit(
-		t.h,
-		C.SCARD_PCI_T1,
-		(*C.BYTE)(&req[0]), reqN, nil,
-		(*C.BYTE)(&resp[0]), &respN)
-	if err := scCheck(rc); err != nil {
-		return false, nil, fmt.Errorf("transmitting request: %w", err)
-	}
-	if respN < 2 {
-		return false, nil, fmt.Errorf("scard response too short: %d", respN)
-	}
-	sw1 := resp[respN-2]
-	sw2 := resp[respN-1]
-	if sw1 == 0x90 && sw2 == 0x00 {
-		return false, resp[:respN-2], nil
-	}
-	if sw1 == 0x61 {
-		return true, resp[:respN-2], nil
-	}
-	return false, nil, &apduErr{sw1, sw2}
 }
 
 func (t *scTx) Transmit(d apdu) ([]byte, error) {
