3ec6e11e562f4cbbd8d8fe4031b8f1c2038ee9c0
Eric Chiang <eric.chiang.m@gmail.com>
2024-07-08 16:27:28 +0000

    update go module path

--- v2/go.mod
+++ v2/go.mod
@@ -1,3 +1,3 @@
-module github.com/go-piv/piv-go
+module github.com/go-piv/piv-go/v2
 
 go 1.16
--- v2/piv/key.go
+++ v2/piv/key.go
@@ -32,7 +32,7 @@ import (
 	"strconv"
 	"strings"
 
-	rsafork "github.com/go-piv/piv-go/third_party/rsa"
+	rsafork "github.com/go-piv/piv-go/v2/third_party/rsa"
 )
 
 // errMismatchingAlgorithms is returned when a cryptographic operation
